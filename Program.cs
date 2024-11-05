b85608c830e80389d75b12457edc29c144d6e8f4
its-michaelroy <72664547+its-michaelroy@users.noreply.github.com>
2024-11-04 19:29:42 -0500

    Setup env

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Assignment1_1_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Assignments 1.1 (Part 1 of 4)
+            Console.WriteLine("Hello, please enter your Name: ");
+            string userName = Console.ReadLine();
+
+            Console.WriteLine("\nPlease enter you age: ");
+            double userAge = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("\nPlease enter your address: ");
+            string userAddress = Console.ReadLine();
+
+
+            Console.WriteLine($"\n\nHello {userName}, you are {userAge} years old. You live at {userAddress}. Nice to meet you!");
+
+            //Assignments 1.1 (Part 2 of 4)
+        }
+    }
+}
