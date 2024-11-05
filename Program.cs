f145afc52d4d061809686219d0f1d4d928b97a20
its-michaelroy <72664547+its-michaelroy@users.noreply.github.com>
2024-11-04 20:45:02 -0500

    Added ! for errors and ReadKey

--- Program.cs
+++ Program.cs
@@ -6,17 +6,18 @@
         {
             //Assignments 1.1 (Part 1 of 4)
             Console.WriteLine("Hello, please enter your Name: ");
-            string userName = Console.ReadLine();
+            string userName = Console.ReadLine()!;
 
             Console.WriteLine("\nPlease enter you age: ");
             double userAge = Convert.ToDouble(Console.ReadLine());
 
             Console.WriteLine("\nPlease enter your address: ");
-            string userAddress = Console.ReadLine();
+            string userAddress = Console.ReadLine()!;
 
 
             Console.WriteLine($"\n\nHello {userName}, you are {userAge} years old. You live at {userAddress}. Nice to meet you!");
-
+            Console.WriteLine("\n\nPress any key to exit...");  
+            Console.ReadKey(intercept: true);
             //Assignments 1.1 (Part 2 of 4)
         }
     }
